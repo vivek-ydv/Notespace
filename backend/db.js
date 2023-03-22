99e210c184b1a6f049fc39cd94071fd1072ae997
Vivek Yadav <vy151617@gmail.com>
2023-03-22 21:39:03 +0530

    Connected to Cloud - Mongo Atlas

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://127.0.0.1:27017/noteApp";
 
-const connectToMongo = () => {
-    mongoose.set('strictQuery', false);
-    mongoose.connect(mongoURI, () => {
-        console.log("Connected to mongoDB sucessfully :)");
-    })
-}
+const mongoURI = "mongodb+srv://vivekydv:vivekCluster0@cluster0.csr861r.mongodb.net/noteSpace?retryWrites=true&w=majority";
+
+const connectToMongo = async () => {
+    try {
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB successfully');
+    } catch (error) {
+        console.log('Error connecting to MongoDB:', error);
+    }
+};
 
 module.exports = connectToMongo;
