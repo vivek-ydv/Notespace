69bde49482482f28b69e5bf94509167da351311a
Vivek Yadav <vy151617@gmail.com>
2023-03-23 13:52:44 +0530

    modified

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,7 @@
 const connectToMongo = require('./db');
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const port = process.env.PORT || 5000;
-
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config({ path: __dirname + '/.env' });
-}
  
 connectToMongo(); // connection to mongoDB
 const app = express();
@@ -21,10 +16,3 @@ app.use('/api/notes', require('./routes/notes'));
 app.listen(port, () => {
     console.log(`Listening on port http://localhost:${port}`);
 })
-
-if (process.env.NODE_ENV == 'production') {
-    app.use(express.static(path.join(__dirname, 'build')));
-    app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    })
-}
