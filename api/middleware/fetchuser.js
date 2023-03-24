1f9afd03ebb4b844c4b0ab264945bdc6eb3c3f64
Vivek Yadav <vy151617@gmail.com>
2023-03-24 11:19:52 +0530

    modified:   api/middleware/fetchuser.js

--- api/middleware/fetchuser.js
+++ api/middleware/fetchuser.js
@@ -4,7 +4,7 @@ const JWT_SECRET = "iti$a$ecret";
 const fetchuser = (req, res, next) => {
     const token = req.header('auth-token');
     if (!token) {
-        res.status(401).send({ error: "Please authenticate using a valid token" });
+        res.status(401).send({ error: "Please authenticate using a valid token-No token found" });
     }
     try {
         const data = jwt.verify(token, JWT_SECRET);
